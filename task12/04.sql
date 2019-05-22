a8d6f64fcb4a513daa09d3e66e0798b1d1f06e2c
Anton144 <anton-paramonov2000@yandex.ru>
2019-05-22 13:22:13 +0300

    sum one line

--- task12/04.sql
+++ task12/04.sql
@@ -4,9 +4,7 @@
 -- данных могут быть страны без городов вообще (например, информации о городах
 -- нет, или кто-то посчитал Антарктиду страной), для таких стран нужно
 -- вывести 0 (0,75 баллов).
-SELECT Country.Name, SUM(CASE WHEN City.Population >= 1000000 THEN 1
-							  ELSE 0
-						END) AS city_pop
+SELECT Country.Name, SUM(CASE WHEN City.Population >= 1000000 THEN 1 ELSE 0 END) AS city_pop
 FROM 
 Country LEFT JOIN City ON Country.Code = City.CountryCode
 GROUP BY Country.Code 
