de717c8fe0e1dc289dbe62b3d98b50f65ea0f0ab
Anton144 <anton-paramonov2000@yandex.ru>
2019-05-22 13:10:09 +0300

    04 replaced count with sum

--- task12/04.sql
+++ task12/04.sql
@@ -4,8 +4,10 @@
 -- данных могут быть страны без городов вообще (например, информации о городах
 -- нет, или кто-то посчитал Антарктиду страной), для таких стран нужно
 -- вывести 0 (0,75 баллов).
-SELECT Country.Name, COUNT(City.Population) AS city_pop
-FROM Country 
-LEFT JOIN City ON Country.Code = City.CountryCode AND City.Population >= 1000000 
+SELECT Country.Name, SUM(CASE WHEN City.Population >= 1000000 THEN 1
+							  ELSE 0
+						END) AS city_pop
+FROM 
+Country LEFT JOIN City ON Country.Code = City.CountryCode
 GROUP BY Country.Code 
 ORDER BY city_pop DESC, Country.Name ASC;
